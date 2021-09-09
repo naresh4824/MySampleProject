fb51f67b6f6a17a1b3801b6b98ea399907a82d27
naresh4824 <naresh4824@gmail.com>
2021-09-09 22:23:22 +0530

    Add files via upload
    Commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  appName: 'Chowkti',
+  appVersion: 'beta 0.1.0',
+  appAuthor: 'Manish',
+  appUrl: 'http://localhost:4200',
+  apiBaseUrl: 'http://localhost:8000',
+};
